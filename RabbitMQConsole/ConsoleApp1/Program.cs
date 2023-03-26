01a39de31e07594715f3e2d2572d7ca7e578332e
Eyüp Birankar <48695028+BirankarE@users.noreply.github.com>
2023-03-26 15:14:34 +0300

    Add files via upload

---
+++ RabbitMQConsole/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+ConnectionFactory factory = new ConnectionFactory();
+//factory.Uri = new Uri("amqp://hkhjerrt:hcqiavAqll6-co4abXnSqUBh_hHifz-Z@hornet.rmq.cloudamqp.com/hkhjerrt");
+factory.HostName = "localhost";
+
+
+using (IConnection connection = factory.CreateConnection())
+using (IModel channel = connection.CreateModel())
+{
+    channel.QueueDeclare("MyQueue",false,false,true);
+    byte[] byteMessage = Encoding.UTF8.GetBytes("bu benim ilk mesajım");
+    channel.BasicPublish(exchange: "", routingKey: "MyQueue", body: byteMessage);
+
+}
---
+++ RabbitMQConsole/Consumer/Program.cs
@@ -0,0 +1,20 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+ConnectionFactory factory = new ConnectionFactory();
+factory.HostName = "localhost";
+
+using (IConnection connection = factory.CreateConnection())
+using (IModel channel = connection.CreateModel())
+{
+    channel.QueueDeclare("MyQueue", false, false, true);
+    EventingBasicConsumer consumer = new EventingBasicConsumer(channel);
+    channel.BasicConsume("MyQueue", false, consumer);
+    consumer.Received += (sender, e) =>
+    {
+        //e.Body : Kuyruktaki mesajı verir.
+        Console.WriteLine(Encoding.UTF8.GetString(e.Body.Span));
+    };
+}
+Console.Read();
